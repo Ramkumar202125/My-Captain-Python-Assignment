d680e8e1d84afe978333fdafb2ae7af6d34ebb2f
Ramkumar202125 <103440009+Ramkumar202125@users.noreply.github.com>
2022-05-03 20:49:18 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,18 @@
+list1=[12,-7,5,64,-14]
+i=len(list1)
+for i in range(0,i):
+    if list1[i]>0:
+        print(list1[i])
+    else:
+        i=i+1
+print("end of list1")
+        
+        
+list2=[12,14,-75,3]
+h=len(list2)
+for j in range(0,h):
+    if list2[j]>0:
+        print(list2[j])
+    else:
+        j=j+1
+    
